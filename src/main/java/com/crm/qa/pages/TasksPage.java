9ca0505c92f8c6c172492beda4f8e9eb7634e1a6
zhanghaoranvivid@gmail.com <zhanghaoranvivid@gmail.com>
2019-01-31 16:24:11 -0500

    first commit

---
+++ src/main/java/com/crm/qa/pages/TasksPage.java
@@ -0,0 +1,11 @@
+package com.crm.qa.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.crm.qa.base.TestBase;
+
+public class TasksPage 	extends TestBase{
+
+	
+}
